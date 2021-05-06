da969cbc11f1fd4ba0d5fead5ca5f56bc1641e56
Basra Abdul Riyaz <basrashaikh45@gmail.com>
2021-05-06 10:17:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Socket } = require('dgram');
 var app=express();
 var http=require('http').Server(app)
 var io=require('socket.io')(http)
-
+//add comments
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
